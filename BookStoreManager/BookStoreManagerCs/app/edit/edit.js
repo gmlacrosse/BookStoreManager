485f8969d327275ae555d69d112afc70ecfdc633
gmlacrosse <mlacrosse3@gmail.com>
2014-02-07 21:57:00 -0600

    updated

---
+++ BookStoreManager/BookStoreManagerCs/app/edit/edit.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'editor';
+    angular.module('app').controller(controllerId,
+        ['$scope', 'common', 'model', 'datacontext', editor])
+    function editor($scope, common, model, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+        var vm = this;
+        vm.book = new model.Book();
+        vm.books = [];
+        vm.saveNewBook = saveNewBook;
+        vm.getBook = getBook;
+        vm.getAllBooks = getAllBooks;
+        vm.deleteBook = deleteBook;
+        active();
+
+        function active(){
+            var promises = [getAllBooks()];
+            common.activateController(promises, controllerId)
+                .then(function() { log('Activated Book Editor')});
+        }
+
+        function saveNewBook() {
+            datacontext.saveNewBook(vm.book).then(function () {
+                vm.book = new model.book();
+            })
+            .then(function () {
+                getAllBooks();
+            });
+        }
+
+        function deleteBook() {
+
+        }
+
+        function getBook() {
+        }
+        
+        function getAllBooks() {
+        }
+    }
+
+})();
